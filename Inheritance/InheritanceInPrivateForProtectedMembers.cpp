21e4a71ca0d8fda8b9ec988386806cf3e7505d2f
mithunmalige <58202638+mithunmalige@users.noreply.github.com>
2024-03-18 19:23:23 +0530

    Inherit in private for protected data members
    - Inherit in private for protected data members
    - Only derived class can access the protected data member.

---
+++ Inheritance/InheritanceInPrivateForProtectedMembers.cpp
@@ -0,0 +1,49 @@
+/******************************************************************************
+Inheritance 2024
+Access specifiers : public inherited : all protected members can be accessed
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+class Base
+{
+    protected:
+    int x;
+};
+
+class Derived1 : private Base
+{
+    protected:
+    int y;
+};
+
+class Derived2 : private Derived1
+{
+    protected:
+    int z;
+    
+    public:
+    void SetValue(int value)
+    {
+        z = value;
+        y = z * 2;   // multiple of 2
+        //x = z * 3;   // error - x is only accessed by its only derived class;
+        
+        cout << "The z value set is " << z << endl;
+        cout << "The y value set is " << y << endl;
+        //cout << "The x value set is " << x << endl;
+    }
+};
+
+void SetValue(Base& base)
+{
+    // base.x = 5; // error - since x is protected it can only accessed by its derived functions
+}
+
+int main()
+{
+    Derived2 d2;
+    d2.SetValue(5); 
+    
+    return 0;
+}
