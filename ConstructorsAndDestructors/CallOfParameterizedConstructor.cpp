ad84ef3785e53203425684705dcd0598217eb74d
mithunmalige <58202638+mithunmalige@users.noreply.github.com>
2024-03-28 11:05:29 +0530

    Call of parameterized constructors
    - Call of parameterized constructors

---
+++ ConstructorsAndDestructors/CallOfParameterizedConstructor.cpp
@@ -0,0 +1,32 @@
+/* Call of constructors : Example with default constructor */
+#include <iostream>
+using namespace std;
+
+class Base
+{
+    public:
+    Base(int a)
+    {
+        cout << a << endl;
+        cout << "Base class called" << endl;
+    }
+};
+
+class Derived : public Base
+{
+    public:
+    Derived(int a) : Base(++a)
+    {
+        cout << a << endl;
+        cout << "Derived class called" << endl;
+    }
+};
+
+int main()
+{
+    // Create derived class object
+    // Derived d; // error
+
+    Derived d(1); // 2, Base class called, 2, Derived class called
+    return 0;
+}
