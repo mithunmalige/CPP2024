be97b26f649d19ea824ca1431076dce9658f74f6
mithunmalige <58202638+mithunmalige@users.noreply.github.com>
2024-04-15 11:40:32 +0530

    Function template definition and normal definition
    - Function template definition and normal definition.
    - Explains how it works.

---
+++ Templates/FunctionTemplateDefinitionAndNormalDefinition.cpp
@@ -0,0 +1,31 @@
+/* Template type - Use both template based and ordinary overloads */
+/* Examples of usage of function templates */
+
+#include <iostream>
+using namespace std;
+
+/* 
+Criteria :
+1. Function shud have same number of arguments
+2. Should do the same functionality
+*/
+
+template <typename T>
+void Show(T a, T b)
+{
+    cout << a << " " << b << endl;
+}
+
+void Show(double a, double b)
+{
+    cout << a << " " << b << endl;
+}
+
+int main()
+{
+    Show(3, 4);     // uses template ones.
+    Show(3.3, 4.4); // uses double ones.
+    Show(4, 5);     // uses template ones.
+
+    return 0;
+}
